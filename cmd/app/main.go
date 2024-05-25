61872c0e7542bd8489d90364907d63a00960b8b2
Andres Court <andres@andrescourt.com>
2024-05-24 22:19:51 -0500

    Configure structured logging by application evironment

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,34 @@
 package main
 
 import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var log *slog.Logger
+
+func init() {
+	var level slog.Level
+	env := os.Getenv("APP_ENV")
+
+	switch env {
+	case "dev":
+		level = slog.LevelDebug
+	case "prod":
+		level = slog.LevelInfo
+	default:
+		level = slog.LevelWarn
+	}
+
+	lh := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+
+	log = slog.New(lh)
+	slog.SetDefault(log)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
